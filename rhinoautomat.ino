6c6e9e1be00ce5181382000eeabcc336853ed32b
Thomas Ebert <thomas.ebert@te-online.net>
2015-07-04 20:33:35 +0200

    Initial commit.

---
+++ rhinoautomat.ino
@@ -0,0 +1,132 @@
+//Attach coinInt to Interrupt Pin 0 (Digital Pin 2). Pin 3 = Interrpt Pin 1.
+const int coinInt = 1; 
+
+//Set the coinsValue to a Volatile float
+//Volatile as this variable changes any time the Interrupt is triggered
+int coinsValue = 0;
+
+//A Coin has been inserted flag
+int coinsChange = 0;
+
+// 50 cent is a 1 that comes from the acceptor
+int coinConst = 50;
+unsigned long lastPulse = 0;
+unsigned long pulseTimeout = 250;
+boolean coinFinished = true;
+boolean waitForCoinFinish = false;
+int currentCoin = 0;
+
+int hopperSensor = A5;
+int hopperPin = 3;
+int hopperSensorTreshold = 100;
+unsigned long coinAt = 0;
+unsigned long hopperTimeout = 1000;
+
+boolean hopper = false;
+
+void setup() {
+  //Start Serial Communication
+  Serial.begin(9600);                 
+  
+  pinMode(6, INPUT);
+
+  //If coinInt goes HIGH (a Pulse), call the coinInserted function
+  //An attachInterrupt will always trigger, even if your using delays
+  attachInterrupt(coinInt, coinInserted, RISING);   
+
+  // Set motor direction to forward
+  pinMode(12, OUTPUT);
+  digitalWrite(12, HIGH);
+  // Disable break
+  pinMode(9, OUTPUT);
+  digitalWrite(9, LOW);
+
+  // Empty hopper on startup
+  emptyHopper();
+}
+
+//The function that is called every time it recieves a pulse
+void coinInserted() {
+  //As we set the Pulse to represent 5p or 5c we add this to the coinsValue
+  coinsValue = coinsValue + coinConst;
+  //Flag that there has been a coin inserted
+  coinsChange = 1;
+
+  // Our machine only recognizes 50cent/1 Euro.
+  // To avoid failures we will hardcode that
+  currentCoin += coinConst;
+  if(currentCoin > 100) {
+    currentCoin = 100;
+    Serial.println("d:too much money detected");
+  }
+  
+  coinFinished = false;
+
+  lastPulse = millis();
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if(analogRead(hopperSensor) < hopperSensorTreshold) {
+    // Coin wurde ausgeworfen
+    coinAt = currentMillis;
+    // elapsed millis ???
+  }
+
+  // Read input from Serial client
+  char input = (char)Serial.read();
+
+  // Command e = eject
+  if(input == 'e') {
+    emptyHopper();
+  }
+
+  // Command p = give current credit
+  if(input == 'p') {
+    Serial.print("p:");
+    Serial.println(coinsValue);
+  }
+
+  // Needed for millis calculation (why?)
+  delay(1);
+  
+  // Hopper is empty
+  if ((unsigned long)(millis() - coinAt) >= hopperTimeout) {
+    // If hopper is switchched on
+    if(hopper == true) {
+      coinAt = millis();
+      hopper = false;
+      coinsValue = 0;
+      // Tell client that hopper is empty
+      Serial.println("e:1");
+    }
+  }
+
+  // Tell client which type of coin was inserted
+  // and reset coin counting
+  if(((unsigned long)(millis() - lastPulse) >= pulseTimeout) && currentCoin > 0) {
+    Serial.print("c:");
+    Serial.println(currentCoin);
+    coinFinished = true;
+    currentCoin = 0;
+  }
+
+  // Activate or deactivate hopper
+  if(hopper == false) {
+    analogWrite(hopperPin, 0);
+  } else {
+    analogWrite(hopperPin, 255);
+  }
+}
+
+// Emptying the hopper
+void emptyHopper() {
+  // Activate hopper
+  if(hopper == false) {
+    coinAt = millis();
+    hopper = true;
+    // Tell client that hopper started
+    Serial.println("e:0");
+  }
+}
